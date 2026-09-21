aefb624dd19a2d9dc254a761ee13b248875f2b58
Mohsin <smrnaqvi887@gmail.com>
2026-09-21 23:28:10 +0530

    loops problem

--- Loops_syntax_code.cpp
+++ Loops_syntax_code.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+/*#include<iostream>
 using namespace std;
 int main() {
     int i=1;  
@@ -8,3 +8,27 @@ int main() {
         i++;
     } return 0;
 }  
+
+#include<iostream>
+using namespace std;
+int main() {
+    int i=2;
+    int n=5;
+    int sum=0;
+    while(i<=n){
+        sum+=i;
+        i+=2;
+    } cout<<""<<sum;
+    return 0;
+}
+    
+#include<iostream>
+using namespace std;
+int main() {
+    int n=5;
+    int sum=0;
+    for(int i=0;i<=n;i+=2){
+        sum+=i;
+    } cout<<""<<sum;
+    return 0;
+}*/
