01bda095b64dff6b9e7caf02a15d8a3c111f2a01
Mohsin <smrnaqvi887@gmail.com>
2026-09-22 23:40:47 +0530

    while rough problem

--- code.cpp
+++ code.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
 using namespace std;
 int main () {
-    int i=10;
-    int n=1;
-    while(i>=n){
+    int i=1;
+    int n=10;
+    while(i<=n){
         cout<< i <<" ";
-        i--;
+        i++;
     } return 0;
 }
